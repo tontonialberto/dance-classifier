7e70851ba40021bc3cee662af018da1c5db9ba03
tontonialberto <tontonialberto@outlook.it>
2024-07-21 13:10:29 +0200

    Add support for viz scripts configuration via json files

--- visualizer.py
+++ visualizer.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 import json
 import time
-    
+import sys
+
+
 def draw_skeleton(img, points, confs, origin_x, origin_y, scale_x, scale_y):
     line_pairs = [
         [5, 6],
@@ -18,12 +20,12 @@ def draw_skeleton(img, points, confs, origin_x, origin_y, scale_x, scale_y):
         [11, 13],
         [13, 15],
         [14, 16],
-        [12, 14]
+        [12, 14],
     ]
-    
+
     scaled_points = points * [scale_x, scale_y]
     scaled_points = scaled_points + [origin_x, origin_y]
-    
+
     for point, scaled_point in zip(points, scaled_points):
         x, y = int(scaled_point[0]), int(scaled_point[1])
         cv2.circle(img, (x, y), 0, (255, 255, 0))
@@ -36,20 +38,21 @@ def draw_skeleton(img, points, confs, origin_x, origin_y, scale_x, scale_y):
             point2 = scaled_points[pair[1]]
             p2_x, p2_y = int(point2[0]), int(point2[1])
             cv2.line(img, (p1_x, p1_y), (p2_x, p2_y), (0, 255, 0))
-            
+
+
 def transform_points(points, confs, box, include_face_poses, scale_to_bounding_box):
     if not include_face_poses:
         # include fictitious face keypoints
         points = np.concatenate([np.zeros((5, 2)), points])
         confs = np.concatenate([np.zeros(5), confs])
-        
+
     if scale_to_bounding_box:
         box_width = box["x2"] - box["x1"]
         box_height = box["y2"] - box["y1"]
         points = (points - [box["x1"], box["y1"]]) / [box_width, box_height]
-        # points_scaled = np.where(points_scaled < 0, 0, points_scaled)
-    
+
     return points, confs
+
 
 def draw_frame(mode, img, track_ids, boxes, all_points, all_confs, origin):
     x = 0
@@ -59,14 +62,33 @@ def draw_frame(mode, img, track_ids, boxes, all_points, all_confs, origin):
     for id, box, points, confs in zip(track_ids, boxes, all_points, all_confs):
         if mode == "poses":
             draw_skeleton(img, points, confs, origin[0], origin[1], 1, 1)
-            cv2.putText(img, str(id), (int(box["x1"] + origin[0] + 10), int(box["y1"] + origin[1] + 10)), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1, color=(0, 255, 0))
+            cv2.putText(
+                img,
+                str(id),
+                (int(box["x1"] + origin[0] + 10), int(box["y1"] + origin[1] + 10)),
+                fontFace=cv2.FONT_HERSHEY_PLAIN,
+                fontScale=1,
+                color=(0, 255, 0),
+            )
         elif mode == "boxes":
-            draw_skeleton(img, points, confs, x + origin[0], y + origin[1], scale_width, scale_height)
-            cv2.putText(img, str(id), (int(x + origin[0] + (scale_width / 2)), int(origin[1] + scale_height)), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1, color=(0, 255, 0))
+            draw_skeleton(
+                img, points, confs, x + origin[0], y + origin[1], scale_width, scale_height
+            )
+            cv2.putText(
+                img,
+                str(id),
+                (int(x + origin[0] + (scale_width / 2)), int(origin[1] + scale_height)),
+                fontFace=cv2.FONT_HERSHEY_PLAIN,
+                fontScale=1,
+                color=(0, 255, 0),
+            )
             x += scale_width
     return img
 
-def draw_video_frame(mode, image, frame_index, frame, origin, scale_to_bounding_box, include_face_poses):
+
+def draw_video_frame(
+    mode, image, frame_index, frame, origin, scale_to_bounding_box, include_face_poses
+):
     track_ids = []
     boxes = []
     all_points = []
@@ -77,52 +99,85 @@ def draw_video_frame(mode, image, frame_index, frame, origin, scale_to_bounding_
             box_width = box["x2"] - box["x1"]
             box_height = box["y2"] - box["y1"]
             if box_width == 0 or box_height == 0:
-                print(f"WARNING: box width or height is 0. Frame {frame_index}, track_id {person.get('track_id')}")
+                print(
+                    f"WARNING: box width or height is 0. Frame {frame_index}, track_id {person.get('track_id')}"
+                )
                 continue
-            
+
             xs = person["keypoints"]["x"]
             ys = person["keypoints"]["y"]
             confs = person["keypoints"]["visible"]
-            
-            points, confs = transform_points(np.array([xs, ys]).T, confs, box, include_face_poses, scale_to_bounding_box)
+
+            points, confs = transform_points(
+                np.array([xs, ys]).T, confs, box, include_face_poses, scale_to_bounding_box
+            )
             all_points.append(points)
             all_confs.append(confs)
             boxes.append(box)
             track_ids.append(person.get("track_id"))
-                
+
     draw_frame(mode, image, track_ids, boxes, all_points, all_confs, origin)
-        
+
+
 def draw_frame_of_multiple_videos(image, videos, frame_index, origins):
     for i, video in enumerate(videos):
         video_name = str(video["path"])
-        # if len(video_name) > 40:
-        #     video_name = video_name[:20] + "..." + video_name[-20:]
-        cv2.putText(image, video_name, (origins[i][0], origins[i][1] + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.25, color=(255, 255, 255))
+        cv2.putText(
+            image,
+            video_name,
+            (origins[i][0], origins[i][1] + 10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.25,
+            color=(255, 255, 255),
+        )
         frame = video["frames"][frame_index]
         scale_to_bounding_box = video["scale"]
         include_face_poses = video["include_face"]
         display_poses = video["display_poses"]
-        # draw_video_frame(image, frame_index, frame, origins[i], scale_to_bounding_box, include_face_poses)
         if display_poses:
-            draw_video_frame("poses", image, frame_index, frame, origins[i], scale_to_bounding_box=False, include_face_poses=include_face_poses)
+            draw_video_frame(
+                "poses",
+                image,
+                frame_index,
+                frame,
+                origins[i],
+                scale_to_bounding_box=False,
+                include_face_poses=include_face_poses,
+            )
         elif video["display_boxes"]:
-            draw_video_frame("boxes", image, frame_index, frame, origins[i], scale_to_bounding_box=scale_to_bounding_box, include_face_poses=include_face_poses)
+            draw_video_frame(
+                "boxes",
+                image,
+                frame_index,
+                frame,
+                origins[i],
+                scale_to_bounding_box=scale_to_bounding_box,
+                include_face_poses=include_face_poses,
+            )
+
 
 def display_videos(videos):
     n_frames = len(videos[0]["frames"])
     all_videos_equal_length = all(n_frames == len(video["frames"]) for video in videos)
-    
+
     if not all_videos_equal_length:
         raise ValueError("All videos must have the same number of frames")
-    
+
     for i in range(n_frames):
         image = np.zeros((720, 1280, 3), np.uint8)
         cv2.line(image, (640, 0), (640, 1280), (255, 255, 255))
         cv2.line(image, (0, 360), (1280, 360), (255, 255, 255))
-        cv2.putText(image, "Frame " + str(i), (0, 355), cv2.FONT_HERSHEY_SIMPLEX, 0.25, color=(255, 255, 255))
-        draw_frame_of_multiple_videos(image, videos, i, [(0,0), (640,0), (0,360), (640,360)])
+        cv2.putText(
+            image,
+            "Frame " + str(i),
+            (0, 355),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.25,
+            color=(255, 255, 255),
+        )
+        draw_frame_of_multiple_videos(image, videos, i, [(0, 0), (640, 0), (0, 360), (640, 360)])
         cv2.imshow("video", image)
-        if (cv2.waitKey(25) & 0xFF) == ord('q'):
+        if (cv2.waitKey(25) & 0xFF) == ord("q"):
             print("frame", i)
             cv2.destroyAllWindows()
             return
@@ -130,55 +185,32 @@ def display_videos(videos):
 def load_config(config_path):
     with open(config_path, "r") as f:
         config = json.load(f)
-    
+
     for i, video in enumerate(config["videos"]):
         assert "path" in video, f"Video path not found in config - at index {i}"
         assert "scale" in video, f"Video scale not found in config - at index {i}"
         assert "include_face" in video, f"Video include_face not found in config - at index {i}"
         assert "display_poses" in video, f"Video display_poses not found in config - at index {i}"
-        assert Path(video["path"]).exists(), f"Video path {video['path']} does not exist - at index {i}"
+        assert Path(
+            video["path"]
+        ).exists(), f"Video path {video['path']} does not exist - at index {i}"
     return config
 
-videos = [
-    {
-        "path": "resources/annotations_medium_dataset/bachata_Bailando Bachata- Chayanne ｜ Daniel y Tom Bachata Groove in Budapest [eN5CCONmJTU].mp4.json",
-        "frames": [],
-        "scale": False,
-        "include_face": True,
-        "display_poses": False,
-        "display_boxes": True,
-    },
-    {
-        "path": "resources/annotations_medium_dataset_linerp_movingavg/bachata_Bailando Bachata- Chayanne ｜ Daniel y Tom Bachata Groove in Budapest [eN5CCONmJTU].mp4.json",
-        "frames": [],
-        "scale": False,
-        "include_face": True,
-        "display_poses": False,
-        "display_boxes": True,
-    },
-    # {
-    #     "path": "resources/annotations_small_dataset/bachata_Janis & Zoé  Bleeding Love - Leona Lewis  Bachata Remix  Bachata Sensual  Couple Dance  Baile.mp4.json",
-    #     "frames": [],
-    #     "scale": True,
-    #     "include_face": True,
-    #     "display_poses": False,
-    #     "display_boxes": True,
-    # },
-    # {
-    #     "path": "resources/annotations_filtered_linear_avg5_minframes60_minvar0.2_minmax/bachata_Daniel & Desirée - ＂Del mar＂ @ Adam Bachata Festival 2021 [m73tKVaGUTY].mp4.json",
-    #     "frames": [],
-    #     "scale": True,
-    #     "include_face": True,
-    #     "display_poses": False,
-    #     "display_boxes": True,
-    # },
-]
 
-for video in videos:
-    with open(video["path"], "r") as f:
-        annotations = json.load(f)
-        video["frames"] = annotations["results"]
+def main(config_path):
+    videos = load_config(config_path)["videos"]
 
-display_videos(videos)
+    for video in videos:
+        with open(video["path"], "r") as f:
+            annotations = json.load(f)
+            video["frames"] = annotations["results"]
 
-cv2.destroyAllWindows()
+    display_videos(videos)
+
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(f"Usage: python {sys.argv[0]} <config_path>")
+        sys.exit(1)
+    main(sys.argv[1])
--- visualizer_sliding_window.py
+++ visualizer_sliding_window.py
@@ -1,8 +1,14 @@
+import json
+from pathlib import Path
+import sys
 import pandas as pd
 import numpy as np
 import cv2
 
-def plot_skeleton(img, points, confs, origin_x, origin_y, include_face_keypoints=True, show_keypoint_numbers=False):
+
+def plot_skeleton(
+    img, points, confs, origin_x, origin_y, include_face_keypoints=True, show_keypoint_numbers=False
+):
     points = np.array(points)
     confs = np.array(confs)
     line_pairs = [
@@ -17,65 +23,104 @@ def plot_skeleton(img, points, confs, origin_x, origin_y, include_face_keypoints
         [11, 13],
         [13, 15],
         [14, 16],
-        [12, 14]
+        [12, 14],
     ]
-    
+
     points += [origin_x, origin_y]
-    
+
     if not include_face_keypoints:
-        points = np.concatenate([[[0,0]] * 5, points])
-    
-    # cv2.rectangle(img, (int(box["x1"]), int(box["y1"])), (int(box["x2"]), int(box["y2"])), (255, 0, 0))
+        points = np.concatenate([[[0, 0]] * 5, points])
+
     for i, point in enumerate(points):
         if point[0] != 0 and point[1] != 0:
             cv2.circle(img, (int(point[0]), int(point[1])), 1, (255, 255, 0))
             if show_keypoint_numbers:
-                cv2.putText(img, str(i+1), (int(point[0]), int(point[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+                cv2.putText(
+                    img,
+                    str(i + 1),
+                    (int(point[0]), int(point[1])),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (255, 255, 255),
+                )
     for pair in line_pairs:
         point1 = points[pair[0]]
         point2 = points[pair[1]]
         if point1[0] != 0 and point1[1] != 0 and point2[0] != 0 and point2[1] != 0:
-            cv2.line(img, (int(points[pair[0]][0]), int(points[pair[0]][1])), (int(points[pair[1]][0]), int(points[pair[1]][1])), (0, 255, 0))
+            cv2.line(
+                img,
+                (int(points[pair[0]][0]), int(points[pair[0]][1])),
+                (int(points[pair[1]][0]), int(points[pair[1]][1])),
+                (0, 255, 0),
+            )
+
+def main(config_path):
+    config = load_config(config_path)
+    video_csv_path = Path(config["path"])
+    df = pd.read_csv(video_csv_path)
     
-    # center_of_gravity_x = np.sum(points.T[0]) / sum(points.T[0] != 0)
-    # center_of_gravity_y = np.sum(points.T[1]) / sum(points.T[1] != 0)
-    # cg = [center_of_gravity_x, center_of_gravity_y]
-    # cv2.circle(img, (int(cg[0]), int(cg[1])), 5, (0, 255, 255))
-    # cv2.putText(img, "CG", (int(cg[0]), int(cg[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255))
+    delay_between_frames = config["delay_between_frames"]
+
+    columns = np.array([])
+    for i in range(1, 31):
+        columns = np.concatenate(
+            [columns, np.array([(f"f{i}_kp{j}_x", f"f{i}_kp{j}_y") for j in range(6, 18)]).flatten()]
+        )
+
+    track_ids = df["track_id"].unique()
+
+    persons = []
+    for track_id in track_ids:
+        persons.append(df[df["track_id"] == track_id])
+
+    max_windows = max([len(person) for person in persons])
+
+    for i in range(max_windows):
+        for k in range(30):
+            img = np.zeros((500, 1300, 3), np.uint8)
+            for j, person in enumerate(persons):
+                if i < len(person):
+                    window = person.iloc[i]
+                    movement = window[columns].values.reshape((30, 12, 2))
+                    x_offset = (j * 100) % 1200
+                    y_offset = (j // 12) * 100
+                    plot_skeleton(
+                        img,
+                        movement[k] * 100,
+                        [1] * 12,
+                        x_offset,
+                        y_offset,
+                        include_face_keypoints=False,
+                    )
+                    description = f"{window['track_id']}, {i}/{len(person)}, {k}"
+                    cv2.putText(
+                        img,
+                        description,
+                        (x_offset, y_offset + 90),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.3,
+                        (255, 255, 255),
+                    )
+            cv2.imshow("window", img)
+            if (cv2.waitKey(delay_between_frames) & 0xFF) == ord("q"):
+                cv2.destroyAllWindows()
+                exit()
+
+    cv2.destroyAllWindows()
     
+def load_config(config_path):
+    with open(config_path, "r") as f:
+        config = json.load(f)
 
-video_csv_path = "resources/medium_dataset_swstep2/dataset/bachata_Bailando Bachata- Chayanne ｜ Daniel y Tom Bachata Groove in Budapest [eN5CCONmJTU].mp4.json.csv"
+    assert "path" in config, f"Video path not found in config"
+    assert "delay_between_frames" in config, f"Video delay_between_frames not found in config"
+    assert Path(
+        config["path"]
+    ).exists(), f"Video path {config['path']} does not exist"
+    return config
 
-df = pd.read_csv(video_csv_path)
-
-columns = np.array([])
-for i in range(1, 31):
-    columns = np.concatenate([columns, np.array([(f"f{i}_kp{j}_x", f"f{i}_kp{j}_y") for j in range(6, 18)]).flatten()])
-
-track_ids = df["track_id"].unique()
-print("track_ids", track_ids)
-
-persons = []
-for track_id in track_ids:
-    persons.append(df[df["track_id"] == track_id])
-    
-max_windows = max([len(person) for person in persons])
-
-for i in range(max_windows):
-    for k in range(30):
-        img = np.zeros((500, 1300, 3), np.uint8)
-        for j, person in enumerate(persons):
-            if i < len(person):
-                window = person.iloc[i]
-                movement = window[columns].values.reshape((30, 12, 2))
-                x_offset = (j * 100) % 1200
-                y_offset = (j // 12) * 100
-                plot_skeleton(img, movement[k] * 100, [1]*12, x_offset, y_offset, include_face_keypoints=False)
-                description = f"{window['track_id']}, {i}/{len(person)}, {k}"
-                cv2.putText(img, description, (x_offset, y_offset + 90), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255))
-        cv2.imshow("window", img)    
-        if (cv2.waitKey(25) & 0xFF) == ord('q'):
-            cv2.destroyAllWindows()
-            exit()
-
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(f"Usage: python {sys.argv[0]} <config_path>")
+        sys.exit(1)
+    main(sys.argv[1])
